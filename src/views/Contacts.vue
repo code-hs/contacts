329b4b52d3a751cadded2fd634e9a6d525590fc1
HS.CODE <foccusstyle@gmail.com>
2020-09-01 14:49:12 +0300

    contact page: button styles change

--- src/views/Contacts.vue
+++ src/views/Contacts.vue
@@ -3,15 +3,31 @@
 		<h1 class="title">Contacts</h1>
 		<div class="content contacts">
 			<div class="contacts__list">
-				<div class="contacts__item">123123123</div>
-				<div class="contacts__item">123123123</div>
-				<div class="contacts__item">123123123</div>
-				<div class="contacts__item">123123123</div>
-				<div class="contacts__item">123123123</div>
-				<div class="contacts__item">123123123</div>
+				<div class="contacts__item">
+					Jon Snow
+					<div class="but-remove" />
+				</div>
+				<div class="contacts__item">
+					Ella Gray
+					<div class="but-remove" />
+				</div>
+				<div class="contacts__item">
+					Madison Rock
+					<div class="but-remove" />
+				</div>
+				<div class="contacts__item">
+					Grayson Carter
+					<div class="but-remove" />
+				</div>
+				<div class="contacts__item">
+					Kinsley Clarke
+					<div class="but-remove" />
+				</div>
 			</div>
 
-			<div class="contacts__but-add">+</div>
+			<div class="contacts__but-add">
+				<img src="/img/icons/add.svg" alt="" />
+			</div>
 		</div>
 	</div>
 </template>
@@ -22,6 +38,7 @@ export default {};
 
 <style scoped>
 .contacts__item {
+	position: relative;
 	color: hsl(0deg 0% 100%);
 	border-bottom: 1px solid hsl(0deg 0% 100% / 45%);
 	height: 55px;
@@ -35,18 +52,34 @@ export default {};
 }
 
 .contacts__but-add {
-	color: hsl(0deg 0% 100%);
 	height: 40px;
-	line-height: 40px;
+	line-height: 44px;
 	cursor: pointer;
 	transition: all 0.5s;
 	border-bottom: none;
-	font-size: 31px;
 	font-weight: bold;
 	font-family: serif;
+	opacity: 0.3;
 }
 
 .contacts__but-add:hover {
-	background: hsl(0deg 0% 0% / 50%);
+	opacity: 1;
+}
+
+.but-remove {
+	opacity: 0;
+	position: absolute;
+	width: 15px;
+	height: 15px;
+	right: 10px;
+	top: 20px;
+	font-family: inherit;
+	background: url(/img/icons/delete.svg) no-repeat center center / contain;
+}
+.contacts__item:hover .but-remove {
+	opacity: 0.2;
+}
+.contacts__item .but-remove:hover {
+	opacity: 1;
 }
 </style>
