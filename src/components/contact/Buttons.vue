1f2f7b344e571cbab332eb20ae011ee4ad70d809
HS.CODE <foccusstyle@gmail.com>
2020-09-01 22:13:16 +0300

    contact page: add back button

---
+++ src/components/contact/Buttons.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="buttons">
+		<router-link to="/" class="back"> </router-link>
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.buttons {
+	display: flex;
+}
+.buttons .back {
+	background: url(/img/icons/back.svg) no-repeat center;
+	cursor: pointer;
+	width: 30%;
+	height: 20px;
+	background-size: contain;
+}
+</style>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,10 +1,19 @@
 <template>
 	<div>
 		<h1 class="title">Name</h1>
-		<div class="content contact">content</div>
+		<div class="content contact">
+			content
+			<Buttons />
+		</div>
 	</div>
 </template>
 
 <script>
-export default {};
+import Buttons from '@/components/contact/Buttons';
+
+export default {
+	components: {
+		Buttons,
+	},
+};
 </script>
